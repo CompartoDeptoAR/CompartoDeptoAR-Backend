54fd1c5643a3f4960f6a7c34486eeb392ab95db1
cramer666 <luciaperrone.unlp@gmail.com>
2025-11-15 20:24:21 -0300

    Solo arregle una ruta, pero lo subo para mantenerlo actualizado y podes usar el codespaces

--- src/routes/PublicacionRutas.ts
+++ src/routes/PublicacionRutas.ts
@@ -3,14 +3,13 @@ import {PublicacionController} from "../controllers/PublicacionControlador";
 import { validarUsuariosRegistrados } from "../middlewares/validarUsuarioRegistrado";
 
 const router = Router();
-
-router.post("/", PublicacionController.crear);
+router.post("/", validarUsuariosRegistrados, PublicacionController.crear);
 router.get("/misPublicaciones",validarUsuariosRegistrados, PublicacionController.misPublicaciones);
 router.get("/", PublicacionController.traerTodas);
 router.get("/:id",PublicacionController.obtenerPorId);
 router.put("/actualizar/:idPublicacion", validarUsuariosRegistrados,PublicacionController.actualizar);
-router.delete("/eliminar/:id",PublicacionController.eliminar);
-router.get("/buscar:texto",PublicacionController.buscar);
+router.delete("/eliminar/:id",PublicacionController.eliminar); // check q sea admin o el q creo la publi
+router.get("/buscar/:texto",PublicacionController.buscar);
 router.post("/buscarConFiltros", PublicacionController.buscarConFiltros);
 
 export default router;
