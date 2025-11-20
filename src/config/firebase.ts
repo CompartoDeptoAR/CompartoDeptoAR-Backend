d727f9ef5678bfa627330af183c32bcc9fbaccf3
cramer666 <luciaperrone.unlp@gmail.com>
2025-11-20 13:51:22 -0300

    Acomode una cosita en la conf de la db

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,11 +4,9 @@ import fs from "fs";
 
 let serviceAccountPath;
 
-// 👇 Primero probamos si existe el secret file en Render
 if (fs.existsSync("/etc/secrets/firebaseKeys.json")) {
   serviceAccountPath = "/etc/secrets/firebaseKeys.json";
 } else {
-  // 👇 Si no existe, usamos el de tu compu (local dev)
   serviceAccountPath = path.resolve("secrets/firebaseKeys.json");
 }
 
@@ -17,14 +15,10 @@ const serviceAccount = require(serviceAccountPath);
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    // databaseURL: "https://PROJECT_ID.firebaseio.com"
   });
 }
 
 export const db = admin.firestore();
 
-// 🔑 Ignorar propiedades undefined
 db.settings({ ignoreUndefinedProperties: true });
 
-
-// export const rtdb = admin.database(); // Para Realtime Database
