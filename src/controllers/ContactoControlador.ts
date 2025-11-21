604f9829ae22d19fdc5c4025ff66fe4648c14fab
cramer666 <luciaperrone.unlp@gmail.com>
2025-11-21 19:55:47 -0300

    contactoController temporar para ver los putos log q no se q pasa

--- src/controllers/ContactoControlador.ts
+++ src/controllers/ContactoControlador.ts
@@ -3,7 +3,7 @@ import { ContactoServicio } from "../services/ContactoServicio";
 
 export class ContactoController {
 
-  static async crear(req: Request, res: Response): Promise<Response> {
+ /* static async crear(req: Request, res: Response): Promise<Response> {
     try {
       const { mail, mensaje } = req.body;
 
@@ -15,7 +15,18 @@ export class ContactoController {
     } catch (err: any) {
       return res.status(500).json({ error: err.message || "Error enviando contacto" });
     }
-  }
+  }*/
+  static async crear(req: Request, res: Response): Promise<Response>{
+      try {
+        console.log("✅ Petición POST /api/contacto recibida. Datos:", req.body);
+        console.log("🚀 Respondiendo 200 de prueba.");
+        return res.status(200).json({ mensaje: "Mensaje de prueba recibido correctamente en el backend." });
+
+      } catch (error) {
+        console.error("❌ ERROR CRÍTICO NO ESPERADO EN ContactoController.crear:", error);
+        return res.status(500).json({ mensaje: "Error interno del servidor (Revisar logs)." });
+      }
+  };
 
   static async listar(req: Request, res: Response): Promise<Response> {
     try {
