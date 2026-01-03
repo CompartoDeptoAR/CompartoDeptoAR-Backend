f333d2834ba132b2398a7f77368a78a8b6791d83
Lucia Perrone <86473981+Cramer666@users.noreply.github.com>
2026-01-03 05:32:15 +0000

    Render tu puta madre <3 x 2

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,6 @@ app.use(cors({}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }))
 app.use(helmet());
-app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("🚀 API funcionando!");
@@ -53,6 +52,8 @@ app.use("/api/calificaciones", CalificacionRutas);
 app.use("/api/moderacion", ModeracionRutas);
 app.use("/api/reportes", ReporteRutas);
 app.use("/api/contacto", ContactoRutas);
+
+app.use(errorMiddleware);
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
